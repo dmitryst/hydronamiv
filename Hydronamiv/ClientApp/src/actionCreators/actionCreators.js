b7e78d86e165162d3ee1ac7b3566ccd21930c700
Dmitry Stepanov <dmitry_stepanov@inbox.ru>
2018-07-23 23:14:26 +0300

    add action creators

---
+++ Hydronamiv/ClientApp/src/actionCreators/actionCreators.js
@@ -0,0 +1,16 @@
+﻿const changeValueType = "CHANGE_VALUE";
+const loadItemsType = "LOAD_ITEMS";
+
+export function changeValue(item) {
+    return {
+        type: changeValueType,
+        items
+    }
+}
+
+export function loadItems(items) {
+    return {
+        type: loadItemsType,
+        items
+    }
+}
